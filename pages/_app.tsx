3254ffc465f924894fde8da49ad3eb022b743da6
wani-nkt <wani.ctf.nkt@gmail.com>
2023-10-03 16:35:09 +0900

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { AppProps } from 'next/app'
 import Head from 'next/head'
 
 import '../styles/global.css';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
